4b484642094d023c2f892be6ac91fb4283011d5f
beatrizsmerino <beatrizsmerino@gmail.com>
2019-04-06 21:43:59 +0200

    Class 1. Instance of vue and text interpolation

---
+++ main.js
@@ -0,0 +1,7 @@
+// Create new instance of vue inside of one variable
+var app = new Vue({ // Pass a object
+    el: '#app', // Element of the DOM controlled by vue
+    data: {
+        message: "Hello fictizia!" // Mensage draw with an interpolation
+    }
+});
